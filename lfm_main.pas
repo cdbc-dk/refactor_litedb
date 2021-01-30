42952be6eae88269895361bedf88d9d0db2417c1
Benny Christensen <bc@localhost.localdomain>
2021-01-30 12:09:46 +0100

    Added test app and litedb class

---
+++ lfm_main.pas
@@ -0,0 +1,39 @@
+unit lfm_main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    btnCreateDb: TButton;
+    Memo1: TMemo;
+    procedure btnCreateDbClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.btnCreateDbClick(Sender: TObject);
+begin
+
+end;
+
+end.
+
